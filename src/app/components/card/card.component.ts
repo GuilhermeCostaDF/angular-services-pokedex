6ac2029a8cd5c787e125ac3c284f31edfd98acc4
GuidoCostaDF <guidocostadf@gmail.com>
2022-11-04 10:31:06 -0300

    component card created; making it smart

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  pokemonName: string = 'Charmander';
+  pokemonPhoto: string = '';
+  pokemonStats: string[] = ['Fire', 'Rock'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
